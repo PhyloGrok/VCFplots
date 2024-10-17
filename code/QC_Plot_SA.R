de07540d4399deb77519295b7a96b0d05d433d80
aicazaga <109315015+aicazaga@users.noreply.github.com>
2024-10-17 14:06:59 -0400

    Add files via upload

--- code/QC_Plot_SA.R
+++ code/QC_Plot_SA.R
@@ -1,32 +1,29 @@
-#Aimee and Ketsia 
-#This code worked for Ketsia S.A QC plot
+#Aimee Icaza 
+#This code is for PA
+# QC calculation scatter plot with logarithmic regression 
 
-#QC calc scatter plot with polynomial regression to the power of 3.5
-#Set up session with the .csv file u are gpoing to use 
-#packages
-packages <- c("tidyverse","ggplot2","ggthemes"
-)
-#Check for dowloaded packages
-for(p in packages){
-  if(!require(p,character.only = TRUE)) install.packages(p)
-  library(p,character.only = TRUE)
+# Load necessary packages
+packages <- c("tidyverse", "ggplot2", "ggthemes")
+
+# Check for and install any missing packages
+for(p in packages) {
+  if(!require(p, character.only = TRUE)) install.packages(p)
+  library(p, character.only = TRUE)
 }
-#opening based on session
-df <- read.csv("QC_EXP.csv")
-class(df)
-#Plot
-#Double check the x and y axis as well as the SRA_Run types with diffrent colors
-ggplot(
-  data = df,
-  mapping = aes(x = Coverage, y = Count._of._SRA_Run )
-) +
-  geom_point(aes(color = SRA_Run)) +
-  geom_smooth(method = "lm", formula = y ~ poly(x,3.5)) +
+
+# Open CSV file
+df <- read.csv("QC_PA.csv")
+
+# Plot: Double check the x and y axes as well as the SRA_Run types with different colors
+ggplot(data = df, mapping = aes(x = Coverage, y = Count.of.SRA_Run)) +
+  #If the count is less than 300 the is is a square (15) is not a crircle (16)
+  geom_point(aes(color = SRA_Run), shape = ifelse(df$Count.of.SRA_Run < 300, 15, 16)) +
+  geom_smooth(method = "lm", formula = y ~ log(x), color = "blue") +
   labs(
     title = "QC Metrics and Coverage",
-    subtitle = expression(italic("Staphylococcus Aureus Coverage by Number of Mutations"),
-    x = "Coverage", y = "Number of Mutations",
+    subtitle = expression(italic("Pseudomonas aeruginosa") ~ "Coverage by Number of Mutations"),
+    x = "Coverage",
+    y = "Number of Mutations",
     color = "SRA_Run"
-  ) 
-#Unique checks the diffrent values in df column SRA_Run
-unique(df$SRA_Run)
+  )
+
