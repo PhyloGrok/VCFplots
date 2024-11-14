4b2b89ff9ab6edd144c5fa53f7313f51c357bf98
Robinson, JM <31963580+PhyloGrok@users.noreply.github.com>
2024-11-14 14:17:23 -0500

    Update Annotation_Impacts_BarPlot.R

--- code/Annotation_Impacts_BarPlot.R
+++ code/Annotation_Impacts_BarPlot.R
@@ -16,6 +16,21 @@ df <- read.csv("../data/P_aeruginosa/QC_PA.csv")
 class(df)
 #Plot
 #Double check the x and y axis as well as the SRA_Run types with diffrent colors
+png(file="../plots/impacts.png")
+ggplot(
+  data = df,
+  mapping = aes(x = Coverage, y = Count.of.SRA_Run )
+) +
+  geom_point(aes(color = SRA_Run)) +
+  geom_smooth(method = "lm", formula = y ~ poly(x,3.5)) +
+  labs(
+    title = "QC Metrics and Coverage",
+    subtitle = expression(italic("Pseudomonas aeruginosa") ~ "Coverage by Number of Mutations",
+    x = "Coverage", y = "Number of Mutations",
+    color = "SRA_Run"
+  ) 
+dev.off()
+
 impactDF <- ggplot(
   data = df,
   mapping = aes(x = Coverage, y = Count.of.SRA_Run )
@@ -28,6 +43,9 @@ impactDF <- ggplot(
     x = "Coverage", y = "Number of Mutations",
     color = "SRA_Run"
   ) 
+
+png(impactDF)
+dev.off()
 
 plot(impactDF)
 #Unique checks the diffrent values in df column SRA_Run
