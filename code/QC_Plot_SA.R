caaf760a52960effccbac9a2bdcaf500d9cf0e9b
aicazaga <109315015+aicazaga@users.noreply.github.com>
2024-11-19 14:47:42 -0500

    Update QC_Plot_SA.R

--- code/QC_Plot_SA.R
+++ code/QC_Plot_SA.R
@@ -15,7 +15,7 @@ for(p in packages) {
 df <- read.csv("QC_PA.csv")
 
 # Plot: Double check the x and y axes as well as the SRA_Run types with different colors
-ggplot(data = df, mapping = aes(x = Coverage, y = Count.of.SRA_Run)) +
+QCplot <- ggplot(data = df, mapping = aes(x = Coverage, y = Count.of.SRA_Run)) +
   #If the count is less than 300 the is is a square (15) is not a crircle (16)
   geom_point(aes(color = SRA_Run), shape = ifelse(df$Count.of.SRA_Run < 300, 15, 16)) +
   geom_smooth(method = "lm", formula = y ~ log(x), color = "blue") +
